633d6bf38683391b8c58e4f6010471ca88b04810
dredgemage <dredgemage@gmail.com>
2016-03-09 09:51:41 -0600

    Clean up and comment

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -24,23 +24,26 @@ import java.util.ArrayList;
 
 public class QuizActivity extends AppCompatActivity {
 
+    // view objects
     private Button mTrueButton;
     private Button mFalseButton;
     private ImageButton mPreviousButton;
     private ImageButton mNextButton;
     private TextView mQuestionTextView;
 
+    // logical objects
     private ArrayList<String> Questions = new ArrayList<>();
     private ArrayList<Boolean> Answers = new ArrayList<>();
     private Question[] mQuestionBank = new Question[11];
-
     private int mCurrentIndex = 0;
 
     private void updateQuestion() {
         Log.d("DEBUG", "updateQuestion called");
+        // update text in TextView to match current question
         String question = Questions.get(mCurrentIndex).substring(1);
         mQuestionTextView.setText(question);
 
+        // save current question in SharedPreferences
         SharedPreferences sharedpreferences = getSharedPreferences("GeoQuizPREFS", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedpreferences.edit();
         editor.putInt("mCurrentIndex", mCurrentIndex);
@@ -50,16 +53,19 @@ public class QuizActivity extends AppCompatActivity {
 
     private void checkAnswer(boolean userPressedTrue) {
         Log.d("DEBUG", "checkAnswer called");
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
+        // preparation
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId;
 
+        // check and set message for toast
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
         } else {
             messageResId = R.string.incorrect_toast;
         }
 
+        // make toast
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show();
         Log.d("DEBUG", "checkAnswer finished");
@@ -67,7 +73,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private void resetQuestions() {
         Log.d("DEBUG", "resetQuestions called");
-        //use AssetManager to get image file names for enabled regions
+
+        //use AssetManager to get file names for enabled region
         AssetManager assets = getAssets();
         try {
             String[] filenames = assets.list("QuestionBank");
@@ -87,18 +94,23 @@ public class QuizActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // separate answer letter from question
         for (String q : Questions) {
             Answers.add(q.substring(0,1).equals("T"));
             q = q.substring(1);
             Log.d("Questions", q);
         }
+
+        /** print answers in debug log
         for (int testingvariable = 0; testingvariable < Questions.size(); testingvariable++){
             if (Answers.get(testingvariable) == Boolean.TRUE) {
                 Log.d("Answers", "True");
             } else {
                 Log.d("Answers", "False");
             }
-        }
+        }**/
+
         Log.d("DEBUG", "resetQuestions finished");
     }
 
@@ -108,10 +120,12 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
         resetQuestions();
 
+        // save questions and answers from resetQuestions to questionBank
         for (int i = 0; i < Questions.size(); i++) {
             mQuestionBank[i] = new Question(Questions.get(i), Answers.get(i));
         }
 
+        // load current index from ShardPreferences if it exists
         SharedPreferences sharedpreferences = getSharedPreferences("GeoQuizPREFS", Context.MODE_PRIVATE);
         int restoredQuestion = sharedpreferences.getInt("mCurrentIndex", 0);
         if (restoredQuestion != 0) {
@@ -119,6 +133,7 @@ public class QuizActivity extends AppCompatActivity {
             Log.d("DEBUG", "GeoQuizPREFS loaded");
         }
 
+        // on click go to next question
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,22 +143,17 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        // on click go to next question
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                checkAnswer(true);
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % Questions.size();
+                updateQuestion();
             }
         });
 
-        mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                checkAnswer(false);
-            }
-        });
-
+        // on click go to previous question
         mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
         mPreviousButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -157,12 +167,21 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        // on click checkAnswer
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                mCurrentIndex = (mCurrentIndex + 1) % Questions.size();
-                updateQuestion();
+            public void onClick(View v) {
+                checkAnswer(true);
+            }
+        });
+
+        // on click checkAnswer
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
             }
         });
 
